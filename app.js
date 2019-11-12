9333353789894a78cd33180faf8954bea25a11a9
Alexanderdunlop <alex.dunlop@orbba.com>
2019-11-12 17:48:03 +1300

    Add guess letter functionality

--- app.js
+++ app.js
@@ -18,7 +18,40 @@ const querySchema = Joi.object({
 });
 
 app.get("/guess/:letter", validator.params(querySchema), (req, res) => {
-  res.send("Got a get guess request");
+  const wordToGuess = "aahing";
+  let guessedLetters = ["a", "b"];
+  let lives = 3;
+  let lettersToGuess = wordToGuess.split("");
+
+  guessedLetters.map(x => {
+    lettersToGuess = lettersToGuess.filter(l => l !== x);
+  });
+
+  let message = "";
+  if (guessedLetters.findIndex(x => x === req.params.letter) > -1) {
+    message = `You have already guessed "${req.params.letter}"`;
+  } else {
+    message = `You successfully guessed "${req.params.letter}"`;
+    guessedLetters.push(req.params.letter);
+
+    const preLetterCount = lettersToGuess.length;
+
+    lettersToGuess = lettersToGuess.filter(x => x !== req.params.letter);
+
+    if (preLetterCount === lettersToGuess.length) {
+      lives -= 1;
+    }
+  }
+
+  res.send({
+    message: message,
+    result: {
+      wordLength: wordToGuess.length,
+      lettersRemaining: lettersToGuess.length,
+      guessedLetters,
+      lives
+    }
+  });
 });
 
 app.listen(port, () => console.log(`Hangman app listening on port ${port}!`));
