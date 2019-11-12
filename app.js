93b635531b6001c35194e188107eb46ca67961e8
Alexanderdunlop <alex.dunlop@orbba.com>
2019-11-12 18:09:56 +1300

    Add words fetching

--- app.js
+++ app.js
@@ -1,12 +1,65 @@
+const fs = require("fs");
 const express = require("express");
 const app = express();
+const https = require("https");
 const Joi = require("joi");
 const validator = require("express-joi-validation").createValidator({});
 
 const port = 3000;
 
+const createGame = res => {
+  res.send("Create game here!");
+};
+
+const fetchWords = res => {
+  https
+    .get(
+      "https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt",
+      response => {
+        let data = "";
+
+        response.on("data", chunk => {
+          data += chunk;
+        });
+
+        response.on("end", () => {
+          let words = data.split("\r\n");
+          words = words.filter(word => word.length >= 3);
+          let json = JSON.stringify({
+            words
+          });
+          fs.writeFile("words.json", json, "utf8", (err, data) => {
+            if (err) {
+              res.send("Sorry an error occured saving words");
+            } else {
+              createGame(res);
+            }
+          });
+        });
+      }
+    )
+    .on("error", e => {
+      console.error(e);
+      res.send("Sorry an error occured fetching word");
+    });
+};
+
 app.get("/start", (req, res) => {
-  res.send("Got a get start request");
+  fs.readFile("words.json", (err, data) => {
+    if (err) {
+      // No words json object, create file.
+      if (
+        err.message === "ENOENT: no such file or directory, open 'words.json'"
+      ) {
+        fetchWords(res);
+      } else {
+        console.error(err);
+        res.send("Sorry an error occured!");
+      }
+    } else {
+      createGame(res);
+    }
+  });
 });
 
 const querySchema = Joi.object({
